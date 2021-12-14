44845ef53216152767d09c7b64cd6636f3984127
molfo <miyabi.miyabiwa@gmail.com>
2021-12-14 22:45:32 +0900

    Threadモデルの作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function threads()
+    {
+        retrurn $this->hasMany(thread::class);
+    }
+    
 }
